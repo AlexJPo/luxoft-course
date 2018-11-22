77427732ca7c075ae006562b429b5ab449ff2075
st13145 <st13145@ptc.local>
2018-11-22 17:46:05 +0300

    update project

--- CardService/src/main/java/com/luxoft/training/spring/cloud/CardRest.java
+++ CardService/src/main/java/com/luxoft/training/spring/cloud/CardRest.java
@@ -1,4 +1,16 @@
 package com.luxoft.training.spring.cloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CardRest {
+    @Autowired
+    private CardNumberGenerator numberGenerator;
+
+    @RequestMapping(path = "/create")
+    public String create() {
+        return numberGenerator.generate();
+    }
 }
--- ProcessingService/src/main/java/com/luxoft/training/spring/cloud/ProcessingRest.java
+++ ProcessingService/src/main/java/com/luxoft/training/spring/cloud/ProcessingRest.java
@@ -1,4 +1,45 @@
 package com.luxoft.training.spring.cloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class ProcessingRest {
+    @Autowired
+    private AccountServiceClient accountServiceClient;
+    @Autowired
+    private CardServiceClient cardServiceClient;
+    @Autowired
+    private ProcessingRepository processingRepository;
+
+    @RequestMapping(path = "/issue/{accountId}")
+    public String issue(@PathVariable Integer accountId) {
+        ProcessingEntity entity = new ProcessingEntity();
+        entity.setAccountId(accountId);
+        entity.setCard(cardServiceClient.create());
+        processingRepository.save(entity);
+        return entity.getCard();
+    }
+
+    @RequestMapping(path = "/checkout/{card}")
+    public boolean checkout(@PathVariable String card, @RequestParam BigDecimal sum) {
+        ProcessingEntity entity = processingRepository.findByCard(card);
+        return entity == null ? false :
+                accountServiceClient.checkout(entity.getAccountId(), sum);
+    }
+
+    @RequestMapping(path = "/get")
+    public Map<Integer, String> get(@RequestParam List<Integer> accounts) {
+        Map<Integer, String> map = new HashMap<>();
+        processingRepository.findByAccountIdIn(accounts).forEach(pe -> map.put(pe.getAccountId(), pe.getCard()));
+        return map;
+    }
 }
